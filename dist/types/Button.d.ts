d4b47d78cbb0810940c409fa648222b78c047788
Austin Johson <127221187+ARJohnsonKwik@users.noreply.github.com>
2024-02-18 11:34:27 -0700

    Delete dist directory

--- dist/types/Button.d.ts
+++
@@ -1,6 +0,0 @@
-interface Props {
-    title: string;
-    onPress: () => void;
-}
-export declare function Button({ title, onPress }: Props): any;
-export {};
--- dist/types/Test.d.ts
+++
@@ -1,5 +0,0 @@
-interface Props {
-    title: string;
-}
-export declare function Test({ title }: Props): import("react/jsx-runtime").JSX.Element;
-export {};
